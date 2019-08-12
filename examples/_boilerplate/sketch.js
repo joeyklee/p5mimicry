dcb89a61fe05b035ed1ccaf953f2421749c16b03
joeyklee <jsphknglee@gmail.com>
2019-08-12 11:30:58 -0400

    added examples

---
+++ examples/_boilerplate/sketch.js
@@ -0,0 +1,9 @@
+
+
+function setup(){
+    createCanvas(480, 360);
+}
+
+function draw(){
+    background(220);
+}
---
+++ examples/spring_interactive/sketch.js
@@ -0,0 +1,21 @@
+
+let spring;
+function setup(){
+    createCanvas(480, 360);
+    spring = new nocjs.Spring(width/2, 20, 100);
+}
+
+function draw(){
+    background(220);
+    spring.bob.drag(mouseX, mouseY);
+    spring.update();
+    spring.display();
+}
+
+function mousePressed(){
+    spring.bob.clicked(mouseX, mouseY);
+}
+
+function mouseReleased(){
+    spring.bob.stopDragging();
+}
