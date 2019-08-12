171f489f009b7f14d7896a8455db832a5ccad5c7
joeyklee <jsphknglee@gmail.com>
2019-08-12 12:11:09 -0400

    update mover examples

---
+++ examples/mover_repel/sketch.js
@@ -0,0 +1,30 @@
+
+let mover;
+let repeller;
+function setup(){
+    createCanvas(480, 360);
+
+    mover = new nocjs.Mover(width/2, height/2, 30);
+    repeller =  new nocjs.Mover(width/2, height/2, 80);
+}
+
+function draw(){
+    background(220);
+    
+    const mouse = createVector(mouseX, mouseY);
+    repeller.location.set(mouse);
+
+    let repulsion = repeller.repel(mover, 'location', 'mass', 2);
+    mover.applyForce(repulsion);
+    
+    mover.update();
+    mover.checkEdges();
+
+    noStroke();
+    fill(  26, 188, 156 );
+    rectMode(CENTER);
+    rect(repeller.location.x, repeller.location.y, 40, 40);
+
+    fill( 142, 68, 173 )
+    mover.display();
+}
---
+++ examples/mover_seek/sketch.js
@@ -0,0 +1,17 @@
+
+let mover;
+function setup(){
+    createCanvas(480, 360);
+
+    mover = new nocjs.Mover(width/2, height/2, 40);
+    mover.maxForce = 1.5;
+}
+
+function draw(){
+    background(220);
+
+    const target = createVector(mouseX, mouseY);
+    mover.seek(target);
+    mover.update();
+    mover.display();
+}
