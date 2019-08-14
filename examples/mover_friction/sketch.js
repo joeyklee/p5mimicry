3eb97291fdbc73d3f29a1f068c43dc9e42d529e9
joeyklee <jsphknglee@gmail.com>
2019-08-13 21:31:18 -0400

    increase speed to prevent sticking

--- examples/mover_friction/sketch.js
+++ examples/mover_friction/sketch.js
@@ -9,7 +9,7 @@ function setup() {
     // movers[0] = new nocjs.Mover(width/2, 10, 40);
     // movers[1] = new nocjs.Mover(20, 10, 40);
 
-    movers = [...new Array(50).fill(null)].map(item => new nocjs.Mover(random(width), 10, 30))
+    movers = [...new Array(50).fill(null)].map(item => new nocjs.Mover(random(width), 10, 30, {maxSpeed:4}))
 
     frictionArea = new FrictionArea(width / 2, height / 2, 100);
 
