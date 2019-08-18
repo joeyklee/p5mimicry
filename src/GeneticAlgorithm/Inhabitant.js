1e9fb99b05a90ccc8a75815331329f6de7db7b5d
joeyklee <jsphknglee@gmail.com>
2019-08-18 18:02:03 -0400

    code update

--- src/GeneticAlgorithm/Inhabitant.js
+++ src/GeneticAlgorithm/Inhabitant.js
@@ -6,9 +6,9 @@ class Inhabitant {
     }
 
     calcFitness() {
-        // this.fitness = 0;
+        // TODO: Fix fitness values - they shouldn't be so outrageously large!
         let sum = this.dna.genes.reduce( (x, y) => x + y);
-        let score = 1 / (sum - this.target);
+        let score = 1/(sum - this.target);
         score = score === Infinity ? 0 : score;
         this.fitness = score;
 
